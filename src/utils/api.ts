317db2251cda675220ba317f5f011a4c75c7c51e
Gabriel Marco <gmarco@fullstacklabs.co>
2024-05-28 10:19:08 -0300

    feat: add .env

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:4000' });
+const api = axios.create({ baseURL: import.meta.env.VITE_API_BASE_URL });
 
 export default api;
